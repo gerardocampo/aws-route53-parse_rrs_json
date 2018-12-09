528601389af34e2cccd9be04768c294a3d2f03ca
Gerard Ocampo <g.c.ocampo@gmail.com>
2018-12-09 14:32:08 -0500

    Factoring in existence of ALIAS and PTR records.

--- parseR53_recordsinHZ.py
+++ parseR53_recordsinHZ.py
@@ -2,7 +2,9 @@
 
 import json
 
-filename = "hz_vitalschoice.com.json"
+# INPUT FILENAME OF JSON TO PARSE BELOW:
+filename = "hz.json"
+
 fh = open(filename, "r")
 data = json.loads(fh.read())
 
@@ -10,15 +12,28 @@ for dnsstuff in data['ResourceRecordSets']:
     record_type = dnsstuff['Type']
     record_name = dnsstuff['Name']
     hostedzoneid = ''
+    is_alias = ''
 
     if record_type == 'CNAME':
-        resource_recs = dnsstuff['ResourceRecords']
-        for cname_records in resource_recs:
-            value = cname_records['Value']
+        if 'AliasTarget' in dnsstuff:
+            is_alias = 'ALIAS'
+            alias_target = dnsstuff['AliasTarget']
+            hostedzoneid = alias_target['HostedZoneId']
+            value = alias_target['DNSName']
+        else:
+            resource_recs = dnsstuff['ResourceRecords']
+            for cname_records in resource_recs:
+                value = cname_records['Value']
     elif record_type == 'A':
-        alias_target = dnsstuff['AliasTarget']
-        hostedzoneid = alias_target['HostedZoneId']
-        value = alias_target['DNSName']
+        if 'AliasTarget' in dnsstuff:
+            is_alias = 'ALIAS'
+            alias_target = dnsstuff['AliasTarget']
+            hostedzoneid = alias_target['HostedZoneId']
+            value = alias_target['DNSName']
+        else:
+            resource_recs = dnsstuff['ResourceRecords']
+            for a_record in resource_recs:
+                value = a_record['Value']
     elif record_type == 'NS':
         resource_recs = dnsstuff['ResourceRecords']
         vals = [d['Value'] for d in resource_recs]
@@ -28,18 +43,39 @@ for dnsstuff in data['ResourceRecordSets']:
         for soa_dict in resource_recs:
             value = soa_dict['Value']
     elif record_type == 'MX':
-        resource_recs = dnsstuff['ResourceRecords']
-        vals = [d['Value'] for d in resource_recs]
-        value = ','.join(vals)
+        if 'AliasTarget' in dnsstuff:
+            is_alias = 'ALIAS'
+            alias_target = dnsstuff['AliasTarget']
+            hostedzoneid = alias_target['HostedZoneId']
+            value = alias_target['DNSName']
+        else:
+            resource_recs = dnsstuff['ResourceRecords']
+            vals = [d['Value'] for d in resource_recs]
+            value = ','.join(vals)
     elif record_type == 'TXT':
-        resource_recs = dnsstuff['ResourceRecords']
-        vals = [d['Value'] for d in resource_recs]
-        value = ' '.join(vals)
+        if 'AliasTarget' in dnsstuff:
+            is_alias = 'ALIAS'
+            alias_target = dnsstuff['AliasTarget']
+            hostedzoneid = alias_target['HostedZoneId']
+            value = alias_target['DNSName']
+        else:
+            resource_recs = dnsstuff['ResourceRecords']
+            vals = [d['Value'] for d in resource_recs]
+            value = ' '.join(vals)
+    elif record_type == 'PTR':
+        if 'AliasTarget' in dnsstuff:
+            is_alias = 'ALIAS'
+            alias_target = dnsstuff['AliasTarget']
+            hostedzoneid = alias_target['HostedZoneId']
+            value = alias_target['DNSName']
+        else:
+            resource_recs = dnsstuff['ResourceRecords']
+            for ptr_record in resource_recs:
+                value = ptr_record['Value']
     else:
-        print('This is a DIFFERENT RECORD TYPE')
-        value = ''
+#        print('This is a DIFFERENT RECORD TYPE')
+        value = 'THIS CODE NEEDS HANDLE THIS RECORD TYPE'
 
-    print("{}\t{}\t{}\t{}\t".format(record_type,record_name,value,hostedzoneid))
+    print("{}\t{}\t{}\t{}\t{}".format(record_type,record_name,is_alias,value,hostedzoneid))
 
 print("\nThere are", len(data['ResourceRecordSets']), "records in this hosted zone.")
-
